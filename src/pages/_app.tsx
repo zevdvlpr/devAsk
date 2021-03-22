f56fcf9801442fe7725fbf0c1e6b7d1a3db65ea7
zevdvlpr <zevdvlpr@gmail.com>
2021-03-22 19:54:55 -0300

    💡 chore: import thme and apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from '@apollo/client';
+
+import { useApollo } from '@lib/apollo/client';
+
+import { colors } from '@config/colors';
+
+import { GlobalStyles } from '@styles/global';
 
 export default function _App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={colors}>
+        <GlobalStyles />
+
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 }

3a05d44863404cdefec305d7fc3bb01b6828b2d1
zevdvlpr <zevdvlpr@gmail.com>
2021-03-22 21:50:22 -0300

    ⚙️ perf: production url

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose, { connect } from 'mongoose';
 
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { NODE_ENV, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
+export const url =
+  NODE_ENV === 'production'
+    ? `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
+    : `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
 export const connectToDatabase = async () => {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
 
-  return mongoose.connect(
-    `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  );
+  return connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
